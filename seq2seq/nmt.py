39fee73fc2fa3858e4cb712640745c1df22182d8
vincentzlt <vincentzlt@gmail.com>
2018-01-08 14:26:12 +0900

    sync

--- seq2seq/nmt.py
+++ seq2seq/nmt.py
@@ -16,47 +16,69 @@ from seq2seq.evaluator import Predictor, Evaluator
 from seq2seq.util.checkpoint import Checkpoint
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--train_path', action='store', dest='train_path',
-                    help='Path to train data')
-parser.add_argument('--dev_path', action='store', dest='dev_path',
-                    help='Path to dev data')
-parser.add_argument('--expt_dir', action='store', dest='expt_dir', default='./experiment',
-                    help='Path to experiment directory. If load_checkpoint is True, then path to checkpoint directory has to be provided')
-parser.add_argument('--load_checkpoint', action='store', dest='load_checkpoint',
-                    help='The name of the checkpoint to load, usually an encoded time string')
-parser.add_argument('--resume', action='store_true', dest='resume',
-                    default=False,
-                    help='Indicates if training has to be resumed from the latest checkpoint')
-parser.add_argument('--log-level', dest='log_level',
-                    default='info',
-                    help='Logging level.')
-TRAIN_PATH='./data/toy_reverse/train/data.txt'
-DEV_PATH='./data/toy_reverse/dev/data.txt'
+parser.add_argument(
+    '--train_path',
+    action='store',
+    dest='train_path',
+    help='Path to train data')
+parser.add_argument(
+    '--dev_path', action='store', dest='dev_path', help='Path to dev data.')
+parser.add_argument(
+    '--expt_dir',
+    action='store',
+    dest='expt_dir',
+    default='./experiment',
+    help=
+    'Path to experiment directory. If load_checkpoint is True, then path to checkpoint directory has to be provided'
+)
+parser.add_argument(
+    '--load_checkpoint',
+    action='store',
+    dest='load_checkpoint',
+    help='The name of the checkpoint to load, usually an encoded time string')
+parser.add_argument(
+    '--resume',
+    action='store_true',
+    dest='resume',
+    default=False,
+    help='Indicates if training has to be resumed from the latest checkpoint')
+parser.add_argument(
+    '--log-level', dest='log_level', default='info', help='Logging level.')
+
+# make a virtual command
+TRAIN_PATH = './data/toy_reverse/train/data.txt'
+DEV_PATH = './data/toy_reverse/dev/data.txt'
 
 opt = parser.parse_args('--train_path {TRAIN_PATH} \
-                        --dev_path {DEV_PATH}'.format(TRAIN_PATH=TRAIN_PATH,
-                                                    DEV_PATH=DEV_PATH).split())
+                        --dev_path {DEV_PATH}'.format(
+    TRAIN_PATH=TRAIN_PATH, DEV_PATH=DEV_PATH).split())
 
+# define logger
 LOG_FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
-logging.basicConfig(format=LOG_FORMAT, level=getattr(logging, opt.log_level.upper()))
+logging.basicConfig(
+    format=LOG_FORMAT, level=getattr(logging, opt.log_level.upper()))
 logging.info(opt)
 
 # Prepare dataset
 src = SourceField()
 tgt = TargetField()
 max_len = 50
+
+
 def len_filter(example):
     return len(example.src) <= max_len and len(example.tgt) <= max_len
+
+
 train = torchtext.data.TabularDataset(
-    path=opt.train_path, format='tsv',
+    path=opt.train_path,
+    format='tsv',
     fields=[('src', src), ('tgt', tgt)],
-    filter_pred=len_filter
-)
+    filter_pred=len_filter)
 dev = torchtext.data.TabularDataset(
-    path=opt.dev_path, format='tsv',
+    path=opt.dev_path,
+    format='tsv',
     fields=[('src', src), ('tgt', tgt)],
-    filter_pred=len_filter
-)
+    filter_pred=len_filter)
 src.build_vocab(train, max_size=50000)
 tgt.build_vocab(train, max_size=50000)
 input_vocab = src.vocab
@@ -65,13 +87,16 @@ output_vocab = tgt.vocab
 # Prepare loss
 weight = torch.ones(len(tgt.vocab))
 pad = tgt.vocab.stoi[tgt.pad_token]
-loss = Perplexity(weight, pad)
+loss = Perplexity(weight=weight, mask=pad)
 if torch.cuda.is_available():
     loss.cuda()
 
 if opt.load_checkpoint is not None:
-    logging.info("loading checkpoint from {}".format(os.path.join(opt.expt_dir, Checkpoint.CHECKPOINT_DIR_NAME, opt.load_checkpoint)))
-    checkpoint_path = os.path.join(opt.expt_dir, Checkpoint.CHECKPOINT_DIR_NAME, opt.load_checkpoint)
+    logging.info("loading checkpoint from {}".format(
+        os.path.join(opt.expt_dir, Checkpoint.CHECKPOINT_DIR_NAME,
+                     opt.load_checkpoint)))
+    checkpoint_path = os.path.join(
+        opt.expt_dir, Checkpoint.CHECKPOINT_DIR_NAME, opt.load_checkpoint)
     checkpoint = Checkpoint.load(checkpoint_path)
     seq2seq = checkpoint.model
     input_vocab = checkpoint.input_vocab
@@ -81,17 +106,25 @@ else:
     optimizer = None
     if not opt.resume:
         # Initialize model
-        hidden_size=128
+        hidden_size = 128
         bidirectional = True
-        encoder = EncoderRNN(len(src.vocab), max_len, hidden_size,
-                             bidirectional=bidirectional,
-                             rnn_cell='lstm',
-                             variable_lengths=True)
-        decoder = DecoderRNN(len(tgt.vocab), max_len, hidden_size * 2,
-                             dropout_p=0.2, use_attention=True,
-                             bidirectional=bidirectional,
-                             rnn_cell='lstm',
-                             eos_id=tgt.eos_id, sos_id=tgt.sos_id)
+        encoder = EncoderRNN(
+            vocab_size=len(src.vocab),
+            max_len=max_len,
+            hidden_size=hidden_size,
+            bidirectional=bidirectional,
+            rnn_cell='lstm',
+            variable_lengths=True)
+        decoder = DecoderRNN(
+            len(tgt.vocab),
+            max_len,
+            hidden_size * 2,
+            dropout_p=0.2,
+            use_attention=True,
+            bidirectional=bidirectional,
+            rnn_cell='lstm',
+            eos_id=tgt.eos_id,
+            sos_id=tgt.sos_id)
         seq2seq = Seq2seq(encoder, decoder)
         if torch.cuda.is_available():
             seq2seq.cuda()
@@ -100,15 +133,21 @@ else:
             param.data.uniform_(-0.08, 0.08)
 
     # train
-    t = SupervisedTrainer(loss=loss, batch_size=32,
-                          checkpoint_every=50,
-                          print_every=10, expt_dir=opt.expt_dir)
+    t = SupervisedTrainer(
+        loss=loss,
+        batch_size=32,
+        checkpoint_every=50,
+        print_every=10,
+        expt_dir=opt.expt_dir)
 
-    seq2seq = t.train(seq2seq, train,
-                      num_epochs=6, dev_data=dev,
-                      optimizer=optimizer,
-                      teacher_forcing_ratio=0.5,
-                      resume=opt.resume)
+    seq2seq = t.train(
+        seq2seq,
+        train,
+        num_epochs=6,
+        dev_data=dev,
+        optimizer=optimizer,
+        teacher_forcing_ratio=0.5,
+        resume=opt.resume)
 
 evaluator = Evaluator(loss=loss, batch_size=32)
 dev_loss, accuracy = evaluator.evaluate(seq2seq, dev)
